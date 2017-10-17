77528e2571216d5aaa4c795d394297bda2cdc36e
StuartARussell <stuartalexanderrussell@gmail.com>
2017-10-17 11:55:50 +0100

    git ignore added

--- db.js
+++ db.js
@@ -1,3 +1,4 @@
 //db.js
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://up785786:31VKSy6nIXVyLMx2@cluster0-shard-00-00-gopoc.mongodb.net:27017,cluster0-shard-00-01-gopoc.mongodb.net:27017,cluster0-shard-00-02-gopoc.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin');
+require('env2')('hidden.env'); // loads all entries into process.env
+mongoose.connect('mongodb://' + process.env.DB_USER + ':' + process.env.DB_PW + '@cluster0-shard-00-00-gopoc.mongodb.net:27017,cluster0-shard-00-01-gopoc.mongodb.net:27017,cluster0-shard-00-02-gopoc.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin');
